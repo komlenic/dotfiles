7a744c7d1a4b41add3682ada170f1f24f6a46b59
Chris Komlenic <komlenic@gmail.com>
2013-12-24 15:05:58 -0500

    Alter makesymlinks.sh to read dotfiles.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -2,6 +2,7 @@
 
 # Store the filename of the executing script.
 this=`basename $0`
+filelist=`ls -A`
 
 echo "This script will rm -rf any already-present files in your home directory.  Continue? (y/n)"
 read a
@@ -9,7 +10,7 @@ if [[ $a == "Y" || $a == "y" ]]
   then
     # For each filename that doesn't match the executing script name
     # or '.git', create a symlink in the executing user's home dir.
-    for file in *
+    for file in $filelist
     do
       if [ "$file" != "$this" -a "$file" != ".git" ]
         then
@@ -18,5 +19,7 @@ if [[ $a == "Y" || $a == "y" ]]
           ln -s "dotfiles/$file" "$HOME/$file"
         fi
     done
+  # Might as well go ahead and source the new .bashrc.
+  source "$HOME/.bashrc"
   fi
 
