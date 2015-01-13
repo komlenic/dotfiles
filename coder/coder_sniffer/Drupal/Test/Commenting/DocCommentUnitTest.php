f472388c1618534c0d149b10cfb0599a49723628
Chris Komlenic <komlenic@gmail.com>
2015-01-13 11:04:46 -0500

    Add non-git version of drupal coder module.

---
+++ coder/coder_sniffer/Drupal/Test/Commenting/DocCommentUnitTest.php
@@ -0,0 +1,43 @@
+<?php
+
+class Drupal_Sniffs_Commenting_DocCommentUnitTest extends CoderSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList($testFile)
+    {
+        return array(
+            12 => 1,
+            14 => 1,
+            16 => 1,
+            17 => 1,
+            18 => 1,
+            19 => 1,
+        );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList($testFile)
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
